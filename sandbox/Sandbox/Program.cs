d8cb61455f20722f830f825a924e708f8f140642
Rylan Hoogland <ryguy8803@gmail.com>
2025-04-23 16:00:18 -0600

    Changed README and Programs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("please enter the radious: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        double area = Math.PI * radius * radius;
+        
+        Console.WriteLine($"Hello {area} Sandbox World!");
+    
     }
 }
